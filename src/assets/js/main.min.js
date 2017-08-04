9de1dd48027d16748ff4d79c96946f2dc55d33d9
poprocki <eugeniusz@iguanastudio.pl>
2017-08-04 18:25:05 +0200

    index, slider, main nav, footer, breadcrumbs, post-item, category page, events page, ui elements page, pagination, tags, form, alerts, sidebar, table

--- src/assets/js/main.min.js
+++ src/assets/js/main.min.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
-	
+
+    // CUSTOM functions
+
+    // mobile dropdown menu
+    $('.top__nav-has-dropdown').on('click', function(){
+        $(this).toggleClass('is-open');
+    });
+
+    // sidebar open 
+    $('.js-sidebar-open').on('click', function(){
+        if($('.sidebar--fixed').length) {
+            $('html').addClass('is-sidebar-open');
+        }
+    })
+    // sidebar close
+    $('.js-sidebar-close').on('click', function(){
+        if($('.sidebar--fixed').length) {
+            $('html').removeClass('is-sidebar-open');
+        }
+    })
+
 	if($.fn.fancybox) {
 		$.extend(true, $.fancybox.defaults, {
 			hash : false
@@ -52,12 +72,12 @@ $(document).ready(function(){
             margin: 0,
             loop: true,
             nav: true,
-            dots: true,
+            dots: false,
             autoplay: true,
             autoplayTimeout: 5000,
-            autoplayHoverPause: false,
+            autoplayHoverPause: true,
             stagePadding: 0,
-            navText: ['Poprzedni','Następny'],
+            navText: ['<i class="ico ico-arrow-left"></i>','<i class="ico ico-arrow-right"></i>'],
             responsiveClass: true,
             responsive: {
                 0:{
@@ -72,6 +92,19 @@ $(document).ready(function(){
             }
         }); 
     }
+
+    // SELECTS
+    
+    // default selected selects add class .is-selected
+    $('select').each(function(){
+        if($(this).find('[selected]').length) {
+            $(this).closest('.custom-select').addClass('is-selected'); 
+        } 
+    })
+    // on click select options add class .is-selected
+    $('select').on('change', function(){
+        $(this).closest('.custom-select').addClass('is-selected');
+    })
 	
 	if (typeof jcf == 'object') {
 		jcf.setOptions('Select', {
@@ -97,16 +130,47 @@ $(document).ready(function(){
 		$(this).css('marginBottom','0').css('marginTop','0').wrap('<div class="table-responsive" style="margin-bottom: '+mB+'; margin-top: '+mT+';"></div>');
 	});
 	
-	$('.menu-trigger').on('click', function(){
-		$('html').toggleClass('is-menu-open');
-	});
+    // menu mobile
+    $('.menu-trigger').on('click', function(){
+        $('html').toggleClass('is-menu-open');
+    });
+    // body on click
+    $('body').on('click touchend', function(e){
+        if(!$(e.target).closest('.menu-trigger').length) {
+          if(!$(e.target).closest('.top__nav').length) {
+            $('html').removeClass('is-menu-open');
+          }
+        }
+        if(!$(e.target).closest('.js-sidebar-open').length) {
+          if(!$(e.target).closest('.sidebar--fixed').length) {
+            $('html').removeClass('is-sidebar-open');
+          }
+        }
+    });
+
+    //
+    // parallax
+    //
+    if(!device.mobile() && !device.tablet() && !$('html').hasClass('ie')) {
+      if($('[data-stellar-background-ratio]').length) {
+        $.stellar({
+            horizontalScrolling: false,
+            verticalOffset: 50
+        });
+      }
+    }
     
 	minimizeTop(); 
 });
 
-$(window).on('load', function(){
+$(window)
+    .scroll(function(){
+        // fade out heading content
+        $('.breadcrumbs-container__inner, .slider__content').css('opacity', 1 - $(window).scrollTop() / 300);
+    })
+    .on('load', function(){
 
-});
+    });
 
 function minimizeTop()
 {
